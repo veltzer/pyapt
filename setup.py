49d22b585b28fa6b512f735485d22568fecf7dc1
Mark Veltzer <mark.veltzer@gmail.com>
2017-10-10 14:38:42 +0300

    setup.py filled

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 import setuptools
 
-import sys
-if not sys.version_info[0] == 3:
-    sys.exit("Sorry, only python version 3 is supported")
-
 setuptools.setup(
     name='pyapt',
     version='0.0.3',
     description='module to help you maintain third party apt repos in a sane way',
     long_description='module to help you maintain third party apt repos in a sane way',
-    url='https://veltzer.github.io/pyapt',
+    url='https://github.com/veltzer/pyapt',
+    download_url='https://github.com/veltzer/pyapt',
     author='Mark Veltzer',
     author_email='mark.veltzer@gmail.com',
+    maintainer='Mark Veltzer',
+    maintainer_email='mark.veltzer@gmail.com',
     license='MIT',
+    platforms=['python3'],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
