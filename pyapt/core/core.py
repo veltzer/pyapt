020b822babcd9857ac690b1fd7e7a241ef72d456
Mark Veltzer <mark.veltzer@gmail.com>
2017-10-17 19:19:13 +0300

    more code

---
+++ pyapt/core/core.py
@@ -0,0 +1,10 @@
+import json
+import os
+
+
+def read_config():
+    file_name = os.path.expanduser("~/.pyapt/config.json")
+    data = json.loads(file_name)
+    return data
+
+def apply_ppa
--- pyapt/scripts/update.py
+++ pyapt/scripts/update.py
@@ -1,6 +1,8 @@
 import click
 from pylogconf import setup
 
+from core.core import read_config
+
 
 @click.command()
 def main():
@@ -9,6 +11,7 @@ def main():
     all required package in the list.
     """
     setup()
+    read_config()
 
 
 if __name__ == "__main__":
