b328285d8be70232bf7a77fe10b3e3f6f4cf4664
Mark Veltzer <mark.veltzer@gmail.com>
2026-08-06 12:09:21 +0300

    pydmt -> rsconstruct

---
+++ config/project.lua
@@ -0,0 +1,7 @@
+NAME = "pyapt"
+DESCRIPTION_SHORT = "module to help you maintain third party apt repos in a sane way"
+KEYWORDS = {
+    "pyapt",
+    "apt",
+    "apt-key",
+}
--- config/project.py
+++
@@ -1,8 +0,0 @@
-""" project definitions """
-name = "pyapt"
-description_short = "module to help you maintain third party apt repos in a sane way"
-keywords = [
-    "pyapt",
-    "apt",
-    "apt-key",
-]
